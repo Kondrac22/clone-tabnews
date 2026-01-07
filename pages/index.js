bae817bddb453e44c4651922cbb7a89c2d45f15b
Leonardo Yamanishi Ferreira <leonardo.yferreira@hotmail.com>
2026-01-07 20:51:43 -0300

    adicionado arquivos de manifesto e pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <h1>MUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+            POh POh
+            muuuuuuuuuuuuuuuuu
+    </h1>
+}
+
+export default Home;
+
